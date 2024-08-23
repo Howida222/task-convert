ba7aece5f98bc3538893a14065c5b70fbddcc0aa
Howida222 <wda192005@gmail.com>
2024-08-24 00:45:41 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,6 @@
+function changeData(){
+    var dollar = document.getElementById("dollar").value;
+    var result = document.getElementById("result");
+    result.innerHTML =dollar *50 + "Egyption Bound";
+    return false;
+}
